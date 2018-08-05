3e9d4d6b1e740705399d84d41d4f8a02caacbc99
Coney Wu <coney.wu@gmail.com>
2018-08-06 01:03:39 +0800

    remove context-path

--- src/main/java/com/thoughtworks/training/wukun/todoservice/client/UserClient.java
+++ src/main/java/com/thoughtworks/training/wukun/todoservice/client/UserClient.java
@@ -5,8 +5,8 @@ import com.thoughtworks.training.wukun.todoservice.dto.User;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient(name = "user",url = "http://localhost:8080")
+@FeignClient(name = "user",url = "http://localhost:8081")
 public interface UserClient {
-    @PostMapping("/api/verifications")
+    @PostMapping("/verifications")
     User verifyToken(String token);
 }
